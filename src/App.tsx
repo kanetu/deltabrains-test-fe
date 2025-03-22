65988a9dcc46b2f98eb5097c5de0eda79524a557
Kane <kanetu731@gmail.com>
2025-03-22 22:54:21 +0700

    redirect index route to event route

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import Footer from "@/components/Footer";
 import Header from "@/components/Header";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import React, { lazy, Suspense } from "react";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
 import "./styles/App.css";
 import "./styles/globals.css";
 import { Toaster } from "./components/ui/Toaster";
@@ -21,6 +21,10 @@ const App: React.FC = () => {
                     <Header />
                     <main className="main flex min-h-[calc(100vh-130px)]">
                         <Routes>
+                            <Route
+                                path="/"
+                                element={<Navigate to="/event" replace />}
+                            />
                             <Route
                                 path="event"
                                 element={
